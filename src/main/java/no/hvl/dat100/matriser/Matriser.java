4d4b48e7797b03eb3430bca94e09722a100805a6
henrikhope12 <henrikhope12@gmail.com>
2025-09-29 14:55:34 +0200

    Update Matriser.java

--- src/main/java/no/hvl/dat100/matriser/Matriser.java
+++ src/main/java/no/hvl/dat100/matriser/Matriser.java
@@ -4,9 +4,12 @@ public class Matriser {
 
 	// a)
 	public static void skrivUt(int[][] matrise) {
-		
-		// TODO
-		throw new UnsupportedOperationException("Metoden skrivUt ikke implementert");
+        for (int[] rad : matrise){
+            for (int verdi : rad) {
+                System.out.print(verdi + " ");
+            }
+            System.out.println();
+        }
 	}
 
 	// b)
@@ -19,17 +22,34 @@ public class Matriser {
 
 	// c)
 	public static int[][] skaler(int tall, int[][] matrise) {
-		
-		// TODO
-		throw new UnsupportedOperationException("Metoden skaler ikke implementert");
+        int[][] nyMatrise = new int[matrise.length][];
+                for(int i = 0; i< matrise.length; i++){
+                    nyMatrise[i] = new int[matrise[i].length];
+                    for (int j = 0; j<matrise[i].length; j++){
+                        nyMatrise[i][j] = matrise[i][j] * tall;
+                    }
+                }
+                return nyMatrise;
+
 	
 	}
 
 	// d)
 	public static boolean erLik(int[][] a, int[][] b) {
-
-		// TODO
-		throw new UnsupportedOperationException("Metoden erLik ikke implementert");
+        if (a.length != b.length){
+            return false;
+        }
+        for (int i = 0; i< a.length; i++){
+            if (a[i].length != b[i].length){
+                return false;
+            }
+            for (int j = 0; j<a[i].length; j++){
+                if (a[i][j] != b[i][j]){
+                    return false;
+                }
+            }
+        }
+        return true;
 		
 	}
 	
